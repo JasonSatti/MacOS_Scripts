4ba939490133c4b35c75d2a209c7421498d2f70f
Dakr-xv <UsmanUllah515@gmail.com>
2018-08-16 17:54:50 -0400

    Replace back ticks with parentheses

--- Management_Scripts/Local_Password_Reminder.sh
+++ Management_Scripts/Local_Password_Reminder.sh
@@ -7,23 +7,23 @@
 LOGGED_IN_USER=`/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys;\
  username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0];\
  username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
-CURRENT_DATE=`date +%s`
+CURRENT_DATE=$(date +%s)
 
-## Get the date that the password was set on in seconds ##
+## Get the date that the password was set on in seconds
 PW_DATE_FLOAT=$(dscl . read /Users/$LOGGED_IN_USER | grep -A 1 passwordLastSetTime | grep -Eo '[0-9.]+')
-PW_DATE_SET=`echo "$PW_DATE_FLOAT/1" | bc`
+PW_DATE_SET=$(echo "$PW_DATE_FLOAT/1" | bc)
 
-## How many seconds has the current password been active ##
+## How many seconds has the current password been active
 DAYS_SECONDS=$(($CURRENT_DATE-$PW_DATE_SET))
 
-## How many days has the current password been active ##
-DAYS_SET=`echo "$DAYS_SECONDS/86400"| bc`
+## How many days has the current password been active
+DAYS_SET=$(echo "$DAYS_SECONDS/86400"| bc)
 
-## Days left till password will expire ##
+## Days left till password will expire
 MAX_PW_AGE=182
 PW_REMAINING_DAYS=$(($MAX_PW_AGE - $DAYS_SET))
 
-## Exit if Password was recently updated ##
+## Exit if Password was recently updated
 if [ $PW_REMAINING_DAYS -gt "177" ]; then
     echo "Password was already updated. Exiting script."
     exit 0;
