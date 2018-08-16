f2176d00ff8e2f4938053e4c301ae2947d6f827a
Dakr-xv <41084708+Dakr-xv@users.noreply.github.com>
2018-08-16 11:50:49 -0400

    Add files via upload

---
+++ Install_Scripts/Virtualbox_Install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+## VirtualBox Installation Script
+## Dakr-xv
+
+## Get the link for latest Virtualbox download
+VIRTUALBOX_DOWNLOAD=$(curl -L https://www.virtualbox.org/wiki/Downloads | grep "OS X hosts" | awk '{print $3}' | cut -f2 -d'"')
+
+## Name of the DMG file that will be downloaded
+VIRTUALBOX_DMG=$(echo $VIRTUALBOX_DOWNLOAD | cut -f6 -d"/")
+
+## Download the latest version of Virtualbox
+curl -O $VIRTUALBOX_DOWNLOAD
+
+## Mount the DMG
+sudo hdiutil attach $VIRTUALBOX_DMG
+
+## Install the PKG
+sudo installer -package /Volumes/VirtualBox/VirtualBox.pkg -target /
+
+## Unmount the DMG
+sudo hdiutil detach /Volumes/VirtualBox
+
+## Remove the DMG
+sudo rm $VIRTUALBOX_DMG
