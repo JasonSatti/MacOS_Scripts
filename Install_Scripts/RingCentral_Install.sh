6a8233f7802e82456f8d7cb2e6437c51556638a9
Dakr-xv <41084708+Dakr-xv@users.noreply.github.com>
2018-09-19 17:48:02 -0400

    Update RingCentral_Install.sh
    Add -s to the curl command

--- Install_Scripts/RingCentral_Install.sh
+++ Install_Scripts/RingCentral_Install.sh
@@ -3,7 +3,7 @@
 ## Dakr-xv
 
 ## Get the link for latest RingCentral download
-RINGCENTRAL_DOWNLOAD=$(curl https://success.ringcentral.com/RCSupportPortalDownloads | grep "Download for MAC" | grep "RCMeetingsClientSetup" | awk '{print $3}' | cut -f2 -d'"')
+RINGCENTRAL_DOWNLOAD=$(curl -s https://success.ringcentral.com/RCSupportPortalDownloads | grep "Download for MAC" | grep "RCMeetingsClientSetup" | awk '{print $3}' | cut -f2 -d'"')
 
 ## Name of the PKG file that will be downloaded
 RINGCENTRAL_PKG=$(echo $RINGCENTRAL_DOWNLOAD | cut -f9 -d'/')
