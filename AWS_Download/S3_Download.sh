d66b1bf2c8aed8080742bfb03b8f6b7a28159e91
Dakr-xv <41084708+Dakr-xv@users.noreply.github.com>
2019-02-20 09:58:20 -0500

    update variables and syntax

--- AWS_Download/S3_Download.sh
+++ AWS_Download/S3_Download.sh
@@ -3,24 +3,31 @@
 ## Dakr-xv
 
 ## Location of file locally
-OUTPUT_FILE="path where you want the file to be downloaded"
+LOCAL_PATH="path where you want the file to be downloaded"
+
 ## Location of file on AWS s3 bucket
-AWS_FILE="path of the file on the aws s3 bucket" 
-## Bucket you want to download from
+AWS_PATH="path of the file on the aws s3 bucket" 
+
+## AWS bucket you want to download from
 AWS_BUCKET="name of the bucket you want to download from"
-RESOURCE="/${AWS_BUCKET}/${AWS_FILE}"
+
+## Setting the resource
+RESOURCE="/${AWS_BUCKET}/${AWS_PATH}"
+
 ## Signature info
 CONTENT_TYPE="application/x-apple-diskimage" 
 DATE=`TZ=GMT date -R`
 STRING_TO_SIGN="GET\n\n${CONTENT_TYPE}\n${DATE}\n${RESOURCE}"
+
 ## AWS key, secret and signature
 S3_KEY="XXXXXXXXXXXXXXXXXX"
 S3_SECRET="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 SIGNATURE=`echo -en ${STRING_TO_SIGN} | openssl sha1 -hmac ${S3_SECRET}\
  -binary | base64`
+
 ## Download File
 curl -H "Host: ${AWS_BUCKET}.s3.amazonaws.com" \
      -H "Date: ${DATE}" \
      -H "Content-Type: ${CONTENT_TYPE}" \
      -H "Authorization: AWS ${S3_KEY}:${SIGNATURE}" \
-     https://${AWS_BUCKET}.s3.amazonaws.com/${AWS_FILE} -o $OUTPUT_FILE
+     https://${AWS_BUCKET}.s3.amazonaws.com/${AWS_PATH} -o $LOCAL_PATH
