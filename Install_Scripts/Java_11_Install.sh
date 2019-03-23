f6eb4e36ca4941ca1c0ee83e6a7edf463292e684
JasonSatti <48844684+JasonSatti@users.noreply.github.com>
2019-03-22 20:04:12 -0400

    Update Java_11_Install.sh

--- Install_Scripts/Java_11_Install.sh
+++ Install_Scripts/Java_11_Install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ## Java 11 Installation Script
-## Dakr-xv
+## Jason Satti
 
 ## Download URL
 JAVA_DOWNLOAD="https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz"
