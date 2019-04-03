2328085bc1cf224363af5b528b63e0742b08a6a1
Jason Satti <48844684+JasonSatti@users.noreply.github.com>
2019-04-03 11:35:26 -0400

    Update Onelogin_Password_Age.py

--- Onelogin_Password_Age/Onelogin_Password_Age.py
+++ Onelogin_Password_Age/Onelogin_Password_Age.py
@@ -1,91 +1,104 @@
 #!/usr/bin/env python3
-# Jason Satti
-import config
-import json
+#Jason Satti
 from datetime import date, datetime
 import requests
 
-
-def calculate_date(password_date):
-    """Return the number of days until the password will expire.
-
-    Expiration time set to 6 months (182 days).
-
-    Calculate current date - password set date then subtract from 182.
-    """
-    password_date_split = password_date.split('T')[0].split('-')
-    password_date_ints = map(int, password_date_split)
-    d_then = date(*password_date_ints)
-    d_now = date(datetime.now().year, datetime.now().month, datetime.now().day)
-    days_set = d_now - d_then
-
-    return (182 - days_set.days)
+import config
 
 
-def return_token(oauth_url, client_id, client_secret):
-    """Return API access token from Onelogin."""
-    r = requests.post(oauth_url,
-                      auth=(client_id, client_secret),
-                      json={"grant_type": "client_credentials"}
-                      )
+class OneloginApi(object):
+    """Connect to a Onelogin instance and retrieve user information."""
 
-    return r.json()['access_token']
+    def __init__(self, oauth_url, url, client_id, client_secret,
+                 password_age_limit=182, removal_set=[]):
+        self.oauth_url = oauth_url
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.token = self.return_token()
+        self.url = url
+        self.removal_set = removal_set
+        self.user_emails = self.get_user_emails()
+        self.password_age_limit = password_age_limit
 
+    def return_token(self):
+        """Return API access token from Onelogin.
 
-def get_user_emails(url, headers, removal_set):
-    """Return all user emails from Onelogin.
+        :return: (dict)
+        """
+        r = requests.post(self.oauth_url,
+                          auth=(self.client_id, self.client_secret),
+                          json={"grant_type": "client_credentials"}
+                          )
 
-    Return all users who have logged in before and are in an active status.
+        return r.json()['access_token']
 
-    Remove all users added to the removal_set above before returning emails.
+    def get_user_emails(self):
+        """Return all active user emails from Onelogin unless they're in the
+        removal set.
 
-    Follow all pagination links for complete list.
-    """
-    user_emails = set()
-    r = requests.get(url, headers=headers)
-    while True:
-        for user in r.json()['data']:
-            if not (user['last_login']) is None and (user['status']) is 1:
-                user_emails.add(user['email'])
-        if 'next' in r.links:
-            url = r.links['next']['url']
-            r = requests.get(url, headers=headers)
-        else:
-            break
+        :return: (list)
+        """
+        user_emails = set()
+        headers = {'Authorization': F'Bearer: {self.token}'}
+        next_url = self.url
+        while next_url:
+            r = requests.get(next_url, headers=headers)
+            for user in r.json()['data']:
+                # Ignore inactive users
+                if not (user['last_login']) is None and (user['status']) is 1:
+                    user_emails.add(user['email'])
+            if 'next' in r.links:
+                next_url = r.links['next']['url']
+            else:
+                next_url = None
 
-    return user_emails.difference(removal_set)
+        return user_emails.difference(self.removal_set)
 
+    def get_password_date(self, email):
+        """Return date of last password change for user.
 
-def get_password_date(prime_url, headers, email):
-    """Return date of last password change for user."""
-    r = requests.get(prime_url, headers=headers,
-                     params={'email': str(email)})
+        :param email: This is a an element returned by get_user_emails()
+        :return: (String)
+        """
+        headers = {'Authorization': F'Bearer: {self.token}'}
+        r = requests.get(self.url, headers=headers,
+                         params={'email': str(email)})
 
-    return r.json()['data'][0]['password_changed_at']
+        return r.json()['data'][0]['password_changed_at']
+
+    def calculate_date(self, user_email, buffer_days=1):
+        """Return the number of days until the password will expire.
+
+        buffer_days - Warn password will expire this many days before actual
+        password expiration date
+
+        :param user_email: This is a an element returned by get_user_emails()
+        :param buffer_days: This is an optional integer value with a default
+        of 1 which can be set when calling calculate_date()
+        :return: (Integer)
+        """
+        password_date = self.get_password_date(user_email)
+        password_age_limit = self.password_age_limit - buffer_days
+        password_date_split = password_date.split('T')[0].split('-')
+        password_date_ints = map(int, password_date_split)
+        d_then = date(*password_date_ints)
+        d_now = date(datetime.now().year, datetime.now().month,
+                     datetime.now().day)
+        days_set = d_now - d_then
+
+        return password_age_limit - days_set.days
 
 
 def main():
-    """Return all users who's password will expire in 7 days.
-
-    Use Onelogin API to gather user email and password set date info.
-
-    Calculate password days remaining per user.
-    """
-    # Save the Onelogin Oath URL, URL and API creds in a config file to import
-    token = return_token(
-        config.oauth_url, config.client_id, config.client_secret)
-    headers = {'Authorization': 'Bearer: {}'.format(token)}
-    prime_url = config.url
-    url = config.url
-    removal_set = config.removal_set
-    # Loop through email array to get password set date for each user
-    # Calculate days until password expiration
-    # Notify if password will expire within 7 days
-    for user in get_user_emails(url, headers, removal_set):
-        password_date = get_password_date(prime_url, headers, user)
-        days_left = calculate_date(password_date)
-        if (days_left <= 7):
-            print '{} days till expiration for {}'.format(days_left, user)
+    """Return all users who's password will expire in 7 days."""
+    addepar_onelogin = OneloginApi(config.oauth_url,
+                                   config.url, config.client_id,
+                                   config.client_secret,
+                                   removal_set=config.removal_set)
+    for user in addepar_onelogin.get_user_emails():
+        days_left = addepar_onelogin.calculate_date(user)
+        if days_left <= 7:
+            print(F'{days_left} days till expiration for {user}')
 
 
 if __name__ == "__main__":
