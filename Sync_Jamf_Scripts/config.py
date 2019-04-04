552963b15ea1f65e7059f31d0d7b395317c89167
Jason Satti <48844684+JasonSatti@users.noreply.github.com>
2019-04-04 11:38:48 -0400

    Add Sync_Jamf_Scripts project

---
+++ Sync_Jamf_Scripts/config.py
@@ -0,0 +1,6 @@
+api_user = 'API_Username'
+api_pw = 'API_Password'
+resource_url = 'JAMF_Resource_URL'
+script_api_url = 'scripts/id/'
+url = resource_url + script_api_url
+script_dir = 'Local_Folder_Name'
