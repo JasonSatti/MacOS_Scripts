ea38dcdb9307851a96952f200fa3ac34b519a104
Jason Satti <48844684+JasonSatti@users.noreply.github.com>
2019-04-12 14:01:52 -0400

    Delete config.py

--- Onelogin_Password_Age/config.py
+++
@@ -1,5 +0,0 @@
-client_id = 'CLIENT_ID'
-client_secret = 'CLIENT_SECRET'
-oauth_url = 'https://api.us.onelogin.com/auth/oauth2/v2/token'
-url = 'https://api.us.onelogin.com/api/1/users'
-removal_set = ['user@company.com']
