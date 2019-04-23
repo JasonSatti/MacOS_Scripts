3cc754754447fac11d782aa2f7bfe9b706cf0486
Jason Satti <48844684+JasonSatti@users.noreply.github.com>
2019-04-23 15:53:13 -0400

    Create Remove_Guest_Network.sh

---
+++ Management_Scripts/Remove_Guest_Network.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# remove_guest_network: Verify corp network exists then remove guest network.
+# Used to verify employees stay connected to corp network
+
+# Set Corp and Guest network SSID names
+CORP_NETWORK="Corp"
+GUEST_NETWORK="Guest"
+
+# Check to see if corp network exists on preffered network list
+NETWORK_CHECK="$(networksetup -listpreferredwirelessnetworks en0 |
+    grep "${CORP_NETWORK}")"
+
+# If corp network does not exist exit script
+# Else attempt to remove guest network
+if [ "${NETWORK_CHECK}" = "" ]; then
+    echo ""${CORP_NETWORK}" SSID Not Found"
+    exit 0
+else
+    echo ""${CORP_NETWORK}" SSID Found, \
+Attempting To Remove "${GUEST_NETWORK}" SSID"
+fi
+
+# Remove guest network
+networksetup -removepreferredwirelessnetwork en0 "${GUEST_NETWORK}"
+exit 0
